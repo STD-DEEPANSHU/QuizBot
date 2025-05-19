3d802f49baa9d5fe24e3ec23b4dfb24805f8219e
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2025-05-19 06:24:40 -1000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const configs = {
-  BOT_TOKEN: process.env.BOT_TOKEN || "7664638373:AAH7T6waS_thYfPjUEXBpPCNFAka_fxoqZo",
-  OWNER_ID: process.env.OWNER_ID || [6107581019],
+  BOT_TOKEN: process.env.BOT_TOKEN || "7984301241:AAGAYjnnPxR1qj9EPegPClAPTuLGHdQ02_w",
+  OWNER_ID: process.env.OWNER_ID || [7711798170],
   MONGO_DB: process.env.MONGO_DB || "mongodb+srv://QuizBot:QuizBot@cluster0.slbm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 };
 
